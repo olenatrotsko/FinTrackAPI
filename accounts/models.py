b926c9755f2e162ce5631f6a19bccf3643d2e266
trolchiha <trotsko.olena@gmail.com>
2024-06-29 21:01:28 +0300

    Created Account Model

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from authentication.models import User
+
+
+class Account(models.Model):
+
+    CURRENCY_CHOICES = [
+        ('USD', 'USD'),
+        ('EUR', 'EUR'),
+        ('GBP', 'GBP'),
+    ]
+
+    name = models.CharField(max_length=50, blank=False, null=False)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, blank=False, null=False)
+    balance = models.DecimalField(max_digits=10, decimal_places=2, blank=False, null=False)
+    is_main = models.BooleanField(default=False, blank=False, null=False)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
