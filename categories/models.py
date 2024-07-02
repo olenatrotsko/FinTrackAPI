0627ffcef65ba05ee946506bfe37be10b3e7238d
trolchiha <trotsko.olena@gmail.com>
2024-07-02 19:40:32 +0300

    Created Category model

--- categories/models.py
+++ categories/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from authentication.models import User
+
+class Category(models.Model):
+
+    CATEGORY_TYPES = (
+        ('income', 'Income'),
+        ('expense', 'Expense')
+    )
+    name = models.CharField(max_length=255)
+    type = models.CharField(max_length=8, choices=CATEGORY_TYPES)
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
