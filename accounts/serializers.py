ea206038b5f721b6f3833989249732838e698776
trolchiha <trotsko.olena@gmail.com>
2024-07-02 18:19:43 +0300

    Added delete method for AccountDetailView; updated Account serializers

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -2,35 +2,44 @@ from rest_framework import serializers
 
 from accounts.models import Account
 
-class AccountSerializer(serializers.ModelSerializer):
+class BaseAccountSerializer(serializers.ModelSerializer):
     class Meta:
         model = Account
         fields = ['id', 'name', 'currency', 'balance', 'is_main']
 
+    def validate_name(self, name):
+        user = self.context['request'].user
+        if Account.objects.filter(user=user, name=name).exists():
+            raise serializers.ValidationError('Account with this name already exists')
+        return name
 
-class CreateUpdateAccountSerializer(serializers.ModelSerializer):
+
+class CreateAccountSerializer(BaseAccountSerializer):
     class Meta:
         model = Account
         fields = ['name', 'currency', 'balance', 'is_main']
 
     def validate(self, attrs):
+        attrs = super().validate(attrs)
+
         user = self.context['request'].user
-        attrs['user'] = user
-        name = attrs.get('name')
         is_main = attrs.get('is_main')
 
-        name_exists = Account.objects.filter(user=user, name=name).exists()
-        if name_exists:
-            raise serializers.ValidationError('Account with this name already exists')
-        
+        attrs['user'] = user
+
         main_account = Account.objects.filter(user=user, is_main=True).first()
-        if is_main and main_account:
-            main_account.is_main = False
-            main_account.save()
+        if main_account and is_main:
+            raise serializers.ValidationError('Main account already exists')
 
         number_of_accounts = Account.objects.filter(user=user).count()
-        if number_of_accounts == 0:
-            attrs['is_main'] = True
+        if number_of_accounts == 0 and not is_main:
+            raise serializers.ValidationError('First account must be main account')
 
-        return super().validate(attrs)
+        return attrs
     
+
+class UpdateAccountSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Account
+        fields = ['id', 'name', 'currency', 'balance', 'is_main']
+        read_only_fields = ['is_main']
--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,11 @@
+from django.shortcuts import get_object_or_404
+
 from rest_framework import views, status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 
 from accounts.models import Account
-from accounts.serializers import AccountSerializer, CreateUpdateAccountSerializer
+from accounts.serializers import BaseAccountSerializer, CreateAccountSerializer, UpdateAccountSerializer
 
 
 class ListAccountsView(views.APIView):
@@ -12,32 +14,39 @@ class ListAccountsView(views.APIView):
 
     def get(self, request):
         accounts = Account.objects.filter(user=request.user).all()
-        serializer = AccountSerializer(accounts, many=True)
+        serializer = BaseAccountSerializer(accounts, many=True)
         return Response(serializer.data)
     
     def post(self, request):
         print(request.user)
-        serializer = CreateUpdateAccountSerializer(data=request.data, context={'request': request})
+        serializer = CreateAccountSerializer(data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
+
 
 class AccountDetailView(views.APIView):
 
     permission_classes = [IsAuthenticated]
 
     def get(self, request, pk):
-        account = Account.objects.filter(user=request.user, id=pk).first()
-        if not account:
-            return Response({'error': 'Account for this user was not found'}, status=status.HTTP_404_NOT_FOUND)
-        serializer = AccountSerializer(account)
+        account = get_object_or_404(Account, user=request.user, id=pk)
+        serializer = BaseAccountSerializer(account)
         return Response(serializer.data)
-    
+
     def put(self, request, pk):
-        account = Account.objects.filter(user=request.user, id=pk).first()
-        serializer = CreateUpdateAccountSerializer(account, data=request.data, context={'request': request})
+        account = get_object_or_404(Account, user=request.user, id=pk)
+        serializer = UpdateAccountSerializer(account, data=request.data, context={'request': request})
         serializer.is_valid(raise_exception=True)
         serializer.save()
         return Response(serializer.data)
     
+    def delete(self, request, pk):
+        account = get_object_or_404(Account, user=request.user, pk=pk)
+
+        if account.is_main:
+            return Response({'error': 'Main account cannot be deleted'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        account.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
     
