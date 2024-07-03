4333c2d0c6b32cee052bea9cf6e2fabc1538083a
trolchiha <trotsko.olena@gmail.com>
2024-07-03 15:10:19 +0300

    Created transactions app and registered in settings

--- categories/views.py
+++ categories/views.py
@@ -21,9 +21,6 @@ class CategoryRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     lookup_field = 'pk'
-
-    def perform_create(self, serializer):
-        return serializer.save(user=self.request.user)
     
     def get_queryset(self):
         return self.queryset.filter(user=self.request.user)
