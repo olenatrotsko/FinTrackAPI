d792b6c50acf8edd594cd0a04c693816ab470ce0
trolchiha <trotsko.olena@gmail.com>
2024-08-09 16:12:16 +0300

    Fixed issue with transaction by category, added total transaction amount views

--- user_stats/urls.py
+++ user_stats/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 
-from user_stats.views import ExpenseCategoryView, IncomeCategoryView
+from user_stats.views import ExpenseCategoryView, IncomeCategoryView, TotalExpenseAmountView, TotalIncomeAmountView
 
 
 urlpatterns = [
-    path('expense-category', ExpenseCategoryView.as_view(), name='expense-category'),
-    path('income-category', IncomeCategoryView.as_view(), name='income-category'),
+    path('expense-by-category', ExpenseCategoryView.as_view(), name='expense-by-category'),
+    path('income-by-category', IncomeCategoryView.as_view(), name='income-by-category'),
+    path('total-expense', TotalExpenseAmountView.as_view(), name='total-expense'),
+    path('total-income', TotalIncomeAmountView.as_view(), name='total-income'),
 ]
--- user_stats/views.py
+++ user_stats/views.py
@@ -3,14 +3,12 @@ from rest_framework import views, status, response, permissions
 from categories.models import Category
 from transactions.models import Transaction
 
-class TransactionCategoryView(views.APIView):
+
+class TransactionByCategoryView(views.APIView):
     permission_classes = [permissions.IsAuthenticated, ]
 
     def get_amount_of_transactions_by_category(self, category, transactions):
-            amount = 0.0
-            for transaction in transactions:
-                if transaction.category == category:
-                    amount += transaction.amount
+            amount = sum([transaction.amount for transaction in transactions if transaction.category == category])
             return amount
         
     def get_categories_by_user_and_type(self, user, type):
@@ -24,7 +22,7 @@ class TransactionCategoryView(views.APIView):
             return data
     
 
-class ExpenseCategoryView(TransactionCategoryView):
+class ExpenseCategoryView(TransactionByCategoryView):
                    
         def get(self, request):
             categories = self.get_categories_by_user_and_type(request.user, "expense")
@@ -33,10 +31,33 @@ class ExpenseCategoryView(TransactionCategoryView):
             return response.Response({"expense_category_data": data}, status=status.HTTP_200_OK)
         
 
-class IncomeCategoryView(TransactionCategoryView):
+class IncomeCategoryView(TransactionByCategoryView):
                    
         def get(self, request):
             categories = self.get_categories_by_user_and_type(request.user, "income")
             incomes = Transaction.objects.filter(user=request.user, category__in=categories).all()
             data = self.get_dict_with_categories_and_amounts(categories, incomes)
             return response.Response({"income_category_data": data}, status=status.HTTP_200_OK)   
+
+
+class TotalTransactionAmountView(views.APIView):
+    permission_classes = [permissions.IsAuthenticated, ]
+
+    def get_total_amount_of_transactions(self, transactions):
+            total_amount = sum([transaction.amount for transaction in transactions])
+            return total_amount
+    
+class TotalExpenseAmountView(TotalTransactionAmountView):
+                   
+        def get(self, request):
+            expenses = Transaction.objects.filter(user=request.user, type="expense").all()
+            total_expense_amount = self.get_total_amount_of_transactions(expenses)
+            return response.Response({"total_expense_amount": total_expense_amount}, status=status.HTTP_200_OK)
+        
+class TotalIncomeAmountView(TotalTransactionAmountView):
+                   
+        def get(self, request):
+            incomes = Transaction.objects.filter(user=request.user, type="income").all()
+            total_income_amount = self.get_total_amount_of_transactions(incomes)
+            return response.Response({"total_income_amount": total_income_amount}, status=status.HTTP_200_OK)
+        
